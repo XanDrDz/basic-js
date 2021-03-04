505b98754f52cb951e60c8ea5e0607479018c1c4
XanDrDz <64421185+XanDrDz@users.noreply.github.com>
2021-03-04 14:17:36 +0500

    feat: add solution extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,26 @@
 const CustomError = require("../extensions/custom-error");
-
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
   
+module.exports = function repeater(str, options) {
+    let addString = String(str);
+    let result = "";
+  
+    if (options.hasOwnProperty("addition")) {
+      addString += String(options.addition);
+    }
+  
+    if (options.hasOwnProperty("additionRepeatTimes")) {
+      for (let i = 1; i < options.additionRepeatTimes; i++) {
+        addString += (options.additionSeparator || "|") + options.addition;
+      }
+    }
+  
+    if (options.hasOwnProperty("repeatTimes")) {
+      for (let i = 1; i < options.repeatTimes; i++) {
+        result += addString + (options.separator || "+");
+      }
+      result += addString;
+    } else {
+      result = addString;
+    }
+    return result;
+  };
